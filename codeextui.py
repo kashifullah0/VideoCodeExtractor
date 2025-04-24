3660cabc8682c611140d925d7663080e0986c346
KashifUllah <kashifullah919@gmail.com>
2025-04-24 07:28:41 +0500

    WIP: local changes before pulling

--- codeextui.py
+++ codeextui.py
@@ -3,8 +3,6 @@ from dotenv import load_dotenv
 import streamlit as st
 from langchain_google_genai import ChatGoogleGenerativeAI
 import os
-
-
 load_dotenv()
 st.title("Video Code Extractor")
 st.caption("Extract code from any type of  coding video in one click")
@@ -16,7 +14,7 @@ if file_upload:
     with open("extracted_text.txt", "r") as f:
         text = f.read()
 
-    llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash",google_api_key=google_api_key)
+    llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
 
     response = llm.invoke(f"I am providing you code, fix it do not add any explanation or comment: {text}").content
 
